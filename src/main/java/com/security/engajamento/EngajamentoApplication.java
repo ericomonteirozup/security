c12c99993ba895b894a6a6fcb30977bc264c8308
Érico Cintra Monteiro <erico.monteiro@zup.com.br>
2020-01-27 11:26:14 -0300

    initial commit

---
+++ src/main/java/com/security/engajamento/EngajamentoApplication.java
@@ -0,0 +1,13 @@
+package com.security.engajamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngajamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngajamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/engajamento/EngajamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.engajamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngajamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
